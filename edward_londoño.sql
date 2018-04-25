c42741f2b17fbd5b493b70857521d6c41f4ef18a
edwardslondon <36299785+edwardslondon@users.noreply.github.com>
2018-04-25 08:18:12 -0500

    Solución parcial

---
+++ edward_londoño.sql
@@ -0,0 +1,95 @@
+--------------------------------------------------------
+--  File created - Wednesday-April-25-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Table CUSTOMERS
+--------------------------------------------------------
+
+  CREATE TABLE "EJERCICIOS"."CUSTOMERS" 
+   (	"ID" NUMBER(*,0), 
+	"NAME" VARCHAR2(255), 
+	"COUNTRY" VARCHAR2(255), 
+	"GRADE" NUMBER(*,0), 
+	"SALESMAN_ID" NUMBER(*,0)
+   )
+--------------------------------------------------------
+--  DDL for Table ORDERS
+--------------------------------------------------------
+
+  CREATE TABLE "EJERCICIOS"."ORDERS" 
+   (	"ID" NUMBER(*,0), 
+	"AMOUNT" NUMBER(8,2), 
+	"ORDER_DATE" DATE, 
+	"CUSTOMER_ID" NUMBER(*,0), 
+	"SALESMAN_ID" NUMBER(*,0)
+   )
+--------------------------------------------------------
+--  DDL for Table SALESMAN
+--------------------------------------------------------
+
+  CREATE TABLE "EJERCICIOS"."SALESMAN" 
+   (	"ID" NUMBER(*,0), 
+	"NAME" VARCHAR2(255), 
+	"COUNTRY" VARCHAR2(255), 
+	"COMMISSION" NUMBER(5,2)
+   )
+--------------------------------------------------------
+--  DDL for View VIEW_1
+--------------------------------------------------------
+
+  CREATE OR REPLACE VIEW "EJERCICIOS"."VIEW_1" ("salesman_name", "customer_name", "country") AS 
+  SELECT SALESMAN.NAME AS "salesman_name", CUSTOMERS.NAME AS "customer_name", SALESMAN.COUNTRY AS "country" FROM SALESMAN INNER JOIN CUSTOMERS 
+     ON CUSTOMERS.SALESMAN_ID = SALESMAN.ID
+     WHERE CUSTOMERS.COUNTRY = CUSTOMERS.COUNTRY
+--------------------------------------------------------
+--  DDL for View VIEW_2
+--------------------------------------------------------
+
+  CREATE OR REPLACE VIEW "EJERCICIOS"."VIEW_2" ("ID", "NAME", "COUNTRY", "GRADE", "SALESMAN_ID", "salesman_name", "commission") AS 
+  SELECT CUSTOMERS."ID",CUSTOMERS."NAME",CUSTOMERS."COUNTRY",CUSTOMERS."GRADE",CUSTOMERS."SALESMAN_ID", SALESMAN.NAME AS "salesman_name", SALESMAN.COMMISSION AS "commission" FROM CUSTOMERS INNER JOIN SALESMAN 
+     ON CUSTOMERS.SALESMAN_ID = SALESMAN.ID
+     WHERE SALESMAN.COMMISSION > 0.12
+--------------------------------------------------------
+--  DDL for Index CUSTOMERS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "EJERCICIOS"."CUSTOMERS_PK" ON "EJERCICIOS"."CUSTOMERS" ("ID")
+--------------------------------------------------------
+--  DDL for Index ORDERS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "EJERCICIOS"."ORDERS_PK" ON "EJERCICIOS"."ORDERS" ("ID")
+--------------------------------------------------------
+--  DDL for Index SALESMAN_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "EJERCICIOS"."SALESMAN_PK" ON "EJERCICIOS"."SALESMAN" ("ID")
+--------------------------------------------------------
+--  Constraints for Table CUSTOMERS
+--------------------------------------------------------
+
+  ALTER TABLE "EJERCICIOS"."CUSTOMERS" ADD CONSTRAINT "CUSTOMERS_PK" PRIMARY KEY ("ID") ENABLE
+--------------------------------------------------------
+--  Constraints for Table ORDERS
+--------------------------------------------------------
+
+  ALTER TABLE "EJERCICIOS"."ORDERS" ADD CONSTRAINT "ORDERS_PK" PRIMARY KEY ("ID") ENABLE
+--------------------------------------------------------
+--  Constraints for Table SALESMAN
+--------------------------------------------------------
+
+  ALTER TABLE "EJERCICIOS"."SALESMAN" ADD CONSTRAINT "SALESMAN_PK" PRIMARY KEY ("ID") ENABLE
+--------------------------------------------------------
+--  Ref Constraints for Table CUSTOMERS
+--------------------------------------------------------
+
+  ALTER TABLE "EJERCICIOS"."CUSTOMERS" ADD CONSTRAINT "SALESMAN_FK" FOREIGN KEY ("SALESMAN_ID")
+	  REFERENCES "EJERCICIOS"."SALESMAN" ("ID") ENABLE
+--------------------------------------------------------
+--  Ref Constraints for Table ORDERS
+--------------------------------------------------------
+
+  ALTER TABLE "EJERCICIOS"."ORDERS" ADD CONSTRAINT "CUSTOMER_ORDER_FK" FOREIGN KEY ("CUSTOMER_ID")
+	  REFERENCES "EJERCICIOS"."CUSTOMERS" ("ID") ENABLE
+  ALTER TABLE "EJERCICIOS"."ORDERS" ADD CONSTRAINT "SALESMAN_ORDER_FK" FOREIGN KEY ("SALESMAN_ID")
+	  REFERENCES "EJERCICIOS"."SALESMAN" ("ID") ENABLE
